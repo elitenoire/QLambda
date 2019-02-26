a4238335f4fa30fe443b21650d47fe87343ea047
Eva Raymond <evathecoderr@outlook.com>
2019-02-26 23:25:06 +0100

    [RAAS] Initial commit

---
+++ examples/react-aws-amplify-starter/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+
+class App extends Component {
+  render() {
+    return <div className="App">Hello</div>;
+  }
+}
+
+export default App;
